b794e4428c245ad9103a91acf5e2738fe68a79b5
MarcHarnist <31386370+MarcHarnist@users.noreply.github.com>
2019-07-08 14:01:14 +0200

    Ajout fichier Date.js: étude des dates en javascript

---
+++ Js/Date.js
@@ -0,0 +1,11 @@
+/**
+ * Etude des dates
+ *
+ */
+
+var noel2018 = new Date(2018,11,25);
+
+var an0 = new Date(1,0,1,12);
+
+console.log(noel2018);
+console.log(an0);
--- Js/tryCatchInJs.js
+++ Js/tryCatchInJs.js
@@ -4,27 +4,36 @@
 var t = "\t";
 
 try {
-	var phrase = "une phrase avec des consonnes et des voyelles"
+	var phrase = "une phrase avec des consonnes et des voyelles";
 	var compteurVoyelles = 0;
     var nombreA = 0;
     var nombreE = 0;
 
     if(estVoyelle(phrase)){
 
+        for(let lettre of phrase){
+        	compteurVoyelles++;
+            switch(lettre){
+                case 'a':
+                    nombreA++;
+                    break;
+                case 'e':
+                    nombreE++
+                    break;
+            }
+        }
         console.log("Il y a %i voyelles ", compteurVoyelles);
         console.log("Il y a %i a ", nombreA);
         console.log("Il y a %i e  ", nombreE);
-        console.log(uneErreur);
-
+        
+        //  console.log(uneErreur);
     }
-
-
-
-
-} catch (e) {
-	console.log("1. Une erreur est survenue.");
+  } catch (e) {
+        console.log("1. Une erreur est survenue.");
 
 }
+  
+  
 console.log("Fin du traitement.")
 
 function estVoyelle() {
