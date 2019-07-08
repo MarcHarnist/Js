ebabada0da9fa32719579e320aa5165e28eea7c0
MarcHarnist <31386370+MarcHarnist@users.noreply.github.com>
2019-07-08 11:50:55 +0200

    First sent

---
+++ Js/Variables_et_Fonctions.js
@@ -0,0 +1,17 @@
+/**
+ *  Hello World
+ *
+ * */
+
+var n = 2;
+var n2 = doubler(n);
+
+function doubler(param1) {
+    var inside = "Hello world"
+    return param1 * 2;
+}
+
+console.log('\n\tLe double de %i est %i', n, n2);
+console.log('var inside')
+console.log("n  ==  %i", n)
+console.log(n)
