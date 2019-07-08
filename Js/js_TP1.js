ab1828a7683f259d70d8173cddcc8d211060b54c
MarcHarnist <31386370+MarcHarnist@users.noreply.github.com>
2019-07-08 15:32:45 +0200

    fin tp1

--- Js/js_TP1.js
+++ Js/js_TP1.js
@@ -5,10 +5,12 @@
  */
 
   // Tableau qui contient les cent nombres
+var ml = "\n\t";
 var cent = [];
 var tableau2 = [];
 var text = "";
-var i;
+
+// On rempli le tableau cent de nombres de 0 à 99
 for (i = 0; i < 100; i++) {
     cent[i] = i;
 }
@@ -16,22 +18,30 @@ for (i = 0; i < 100; i++) {
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
-// Nombre aléatoire
-var nombreAleatoire = getRandomInt(100);
 
-//Si le nombre n'appartient pas au tableau2 à afficher on l'ajoute
-for(i=0; i<cent.length; i++)
+// Ajoute le nombre aléatoire à tableau2 tant que le tableau2 a moins de 100 case
+while(tableau2.length < 100 )
 {
-    if(cent[i] == nombreAleatoire)
+
+    // Nombre aléatoire
+    let nombreAleatoire = getRandomInt(100);
+
+    //Si le nombre aléatoire n'appartient pas au tableau2 on l'y ajoute
+    if (tableau2.indexOf(nombreAleatoire) < 0) //Si le retour est -1 on l'ajoute
     {
-        tableau2.push(nombreAleatoire);
+        tableau2.push(nombreAleatoire);//ajoute le nombre aléatoire au tableau
     }
+
 }
+
+
+//Affiche la taille de cent[]
+console.log(ml + "Taille de cent = %i", cent.length)
 
 //affiche le contenu du tableau2
 for(let pas of tableau2)
 {
-    console.log(pas);
+    console.log(ml+"boucle foreach donne " + pas);
 }
 
 
